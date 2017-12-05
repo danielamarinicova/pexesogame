a4c8bd495a91633b4795e19cbfcd4cc27401401d
Daniela Marinicova <marinicova.d@gmail.com>
2017-12-05 10:19:55 +0100

    game

--- script/script.js
+++ script/script.js
@@ -1,6 +1,9 @@
 var player=1;
+var score1=0;
+var score2=0;
 var turn1=-1;
 var turn2=-1;
+var turnGuessed;
 var field = [ [1,1,2,2], [3,3,4,4], [5,5,6,6], [7,7,8,8] ];
 shuffle();
 
@@ -17,17 +20,22 @@ function shuffle()
 		field[r1][c1]=field[r2][c2];
 		field[r2][c2]=temp;
 	}
-
+console.log("Shuffle Finished:");
+console.log(field);
 
 }
 
 
-function pos(value) {
+function clickImg(value) {
 	if(turn2!=-1)
 		return;
 	var row=Math.floor(value/10);
 	var col=value%10;
-	console.log("You clicked: row:"+row+" col:"+col) ;
+
+	if(field[row][col]==0)
+		return;
+console.log("You clicked: row:"+row+" col:"+col) ;
+
 	if(turn1==row*10+col)
 		return;
 
@@ -52,15 +60,26 @@ function checkSelectedCards (){
 	var col2=turn2%10;
 
 	if(field[row1][col1]==field[row2][col2]){
-
-											}
-	else{
-		setTimeout (turnBack, 3000);
+		if (player==1){
+			score1++;
 		}
+
+											
+	else{
+		score2++;
+		}
+		turnGuessed=turn1;
+		setTimeout(moveCard,1500);
+		updateScore();
 }
-
-
+else
+{
+setTimeout(turnBack,2000);
+player=player==1?2:1;
+}
+}
 function turnBack(){
+	console.log("test");
 	var row1=Math.floor(turn1/10);
 	var row2=Math.floor(turn2/10);
 	var col1=turn1%10;
@@ -69,8 +88,46 @@ function turnBack(){
 	console.log(imageId) ;
 	document.getElementById(imageId).src="images/back.jpg";	
 	var imageId="img"+row2+col2;
-	console.log(imageId) ;
 	document.getElementById(imageId).src="images/back.jpg";
 	turn1=-1;
 	turn2=-1;
+}
+
+function moveCard(){
+	var table;
+	if(player==1){
+		table=document.getElementById("tableP1");
+	}else{
+		table=document.getElementById("tableP2");
+	}
+	var row1=Math.floor(turn1/10);
+	var col1=turn1%10;
+	var row2=Math.floor(turn2/10);
+	var col2=turn2%10;
+	var imageSource="images/img"+field[row1][col1]+".jpg";
+
+	var imageId="img"+row1+col1;
+	document.getElementById(imageId).src="images/blank.jpg";
+
+	imageId="img"+row2+col2;
+	document.getElementById(imageId).src="images/blank.jpg";
+	var row = table.insertRow(0);
+
+	var cell=row.insertCell(0);
+	cell.innerHTML="<IMG SRC=\""+imageSource+"\" Width=\"75\">";
+
+	field[row1][col1]=0;
+	field[row2][col2]=0;
+
+	turn1=-1;
+	turn2=-1;
+}
+
+function updateScore(){
+	if(player==1){
+		document.getElementById("score1").innerHTML="Score :"+score1;
+	}
+	else{
+		document.getElementById("score2").innerHTML="Score :"+score2;
+	}
 }
